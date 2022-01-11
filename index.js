b2f1e11c6f7bd6be0b0a7275f6f4cd1c9597545f
ceciliarr <cecilia.romeroldan@gmail.com>
2022-01-10 21:25:58 -0300

    scss tarjetas categorias

--- index.js
+++ index.js
@@ -19,26 +19,53 @@ const navDeporte = document.querySelector(".deporte");
 
 
 
-navCelulares.onclick = () => {
-    console.log("celulares");
+/* navCelulares.onclick = () => {
+    console.log("celulares"); */
     fetch("https://api.mercadolibre.com/sites/MLA/search?q=celular")
     .then(res => res.json())
     .then((data) => {
     console.log(data);
     crearTarjeta(data.results)
 })    
-}
+//}
 
 const crearTarjeta = (data) => {
     const tarjetasCelulares = document.querySelector(".tarjetas-celulares");
     const html = data.reduce ((acc, curr) => {
         return acc + `
             <article class = "tarjetas-categoria">
-                <h2>${curr.title}</h2>
-                <img src="${curr.thumbnail}">
-                <p>$ ${curr.price}</p>
+                
+                <div class="img-tarjetas">
+                 <img src="${curr.thumbnail}">
+                </div>
+                <div class="info-tarjetas">
+                <h3>${curr.title}</h3>
+                <h4>$ ${curr.price}</h4>
+                </div>
             </article> 
         `
     }, "")
     tarjetasCelulares.innerHTML = html
-}
+} 
+
+
+
+
+/* const crearTarjeta = (data) => {
+    const tarjetasCelulares = document.querySelector(".tarjetas-celulares");
+    html = ""
+    for (let index = 0; index < 4; index++) {
+    return html + `
+    <article class = "tarjetas-categoria">
+        <h2>${data.title}</h2>
+        <div class="img-tarjetas">
+         <img src="${data.thumbnail}">
+        </div>
+        <p>$ ${data.price}</p>
+    </article> 
+        `
+    
+    }
+    tarjetasCelulares.innerHTML = html
+} */
+
